59fe7911246a9ac192c1f7034c9044b22ac0da71
triploit <tristanpieper080803@gmail.com>
2016-09-25 18:52:26 +0200

    Updated!

--- Interpreter/install.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo "@java -jar C:\Windows\System32\ny++.jar %*">C:\Windows\System32\nypp.bat
-move jar/ny++.jar C:\Windows\System32\ny++.jar
--- src/com/github/triploit/npp5/other/CompileCode.java
+++ src/com/github/triploit/npp5/other/CompileCode.java
@@ -20,13 +20,13 @@ public class CompileCode
 	{
 		lc.addString("mov",    "_0d8dnBK34tv3vq3b2b673753nbzgu7hj5gftrJd9d8dgbaksj");
 		lc.addString("add",    "_dmwjl4345trz55t6zeruzez5t4r65rzk88zbw67ard7gb");
-		lc.addString("defi",   "ankadj4z6z7huj546ez5tu6zrte4w5e6rzuiwhdkwndknawkj_");
-		lc.addString("defs",   "abd89baxtrg4eq23trerw45trzcu87w/Tgb8)TDh9_");
+		lc.addString("defi",   "_bf0nefbrvbv3bq3v39q4teufrz44z8urwiefhzu423");
+		lc.addString("defs",   "_zuhefie8978tgzruhfwjioc98f7t67f2gehuded7f878zhui");
 		lc.addString("div",    "_fr234e44rtt32rtw43egz5hrwe4tzuisa8u7r1e2dq2rr12");
 		lc.addString("leq",    "_qr123ot687z8u6t7zd45rsw35setrhzdtjuzfq3fq");
 		lc.addString("geq",    "_2qf2r6u7t6i85674e63wa5erfht6u7e4fq2r2");
-		lc.addString("eq",     "_qf2fqetrhztjgmzku7t6i58e47w36segt2f2tf");
-		lc.addString("neq",    "_q2tg28juuuuuuuuxdfcgbjzu7t65e4df3gerh");
+		lc.addString("eq",     "_9vq3rn0v903vb7vn3bvnr373vqvrvqn37vz0v980203vn");
+		lc.addString("neq",    "_q2tg28jwfjpfapr90b0xdfcgbjzu7t65e4df3gerh");
 		lc.addString("mod",    "_wafawfz678dgtrfh7zugfr6z5ezh4w3eftjna");
 		lc.addString("mul",    "_z234t62t65rsdfxbgnjzu5retrsgf43gfbhw2");
 		lc.addString("div",    "_5u7z62wrdhgfhjzuztaegsxfcgnvhgkzuiwht3rf3e");
@@ -47,6 +47,150 @@ public class CompileCode
 		lc.addString(" ",      "_nw0faqrfvnzm9q087rnfzh0viihdgztn0wf60fn7a90w7fn");
 		lc.addString("\"",     "_2w3e45r5qrwfnz9e8r7gt83nvrg9097mt6zi7ri5u8etj");
 		lc.addString(",",      "_w3zu487wzrfnqz98t89q7nbu9rzq78zhsrjrk44zu5hj5i");
+		
+		/*lc.addString("A", "_3bv98rh0qnpv2b12787z6t5frhzuji87zuih90nrh8n90bv7rn3");
+		lc.addString("E", "_q2r7nzu90mjj45io0pobhzgt6z7890iopkjk2ß6z3q90v03nr0r9389n");
+		lc.addString("I", "_66g4w89u98u9f0uh89u394tz8i907z87uiuhuzguztzre3ui");
+		lc.addString("O", "_e3d4rf5tg6hzjukilopo9kki8ju7hzbgtvfvrcvrgh6j7zu89o");
+		lc.addString("U", "_45rtfhzuji876u5trfgd5rtzui8976tzuihjgftz7654et76zu8ihjgtz76");
+		lc.addString("R", "_893gb88fz4864guqr9203g3r9qrp3hg3zrqp0247fn895gbth8");
+		lc.addString("L", "_efzuhivvnh0qvznq390bgrqnevhwunh75h29z84n9t4z90q3bwe4");
+		lc.addString("G", "_fe07gu9rqwh76fn0qmßwhz4tbqh4vokouifnu6305ribt84z");
+		lc.addString("H", "_54uzhe4gbt2q23rwebg35tnb345bt4n6j53q5tnj5hrq3h5g");
+		lc.addString("S", "_3h35th4erdthgw4gert656454werdftzmju765h4gerwf5ju6");
+
+		/*lc.addString("A", " 01 ");
+		lc.addString("a", " 02 ");
+		
+		lc.addString("B", " 03 ");
+		lc.addString("b", " 04 ");
+		
+		lc.addString("C", " 05 ");
+		lc.addString("c", " 06 ");
+		
+		lc.addString("D", " 07 ");
+		lc.addString("d", " 08 ");
+		
+		lc.addString("E", " 09 ");
+		lc.addString("e", " 0A ");
+		
+		lc.addString("F", " 0B ");
+		lc.addString("f", " 0C ");
+		
+		lc.addString("G", " 0D ");
+		lc.addString("g", " 0E ");
+		
+		lc.addString("I", " 0F ");
+		lc.addString("i", " A1 ");
+		
+		lc.addString("J", " A2 ");
+		lc.addString("j", " A3 ");
+		
+		lc.addString("K", " A4 ");
+		lc.addString("k", " A5 ");
+		
+		lc.addString("L", " A6 ");
+		lc.addString("l", " A7 ");
+		
+		lc.addString("M", " A8 ");
+		lc.addString("m", " A9 ");
+		
+		lc.addString("N", " AA ");
+		lc.addString("n", " AB ");
+		
+		lc.addString("O", " AC ");
+		lc.addString("o", " AD ");
+		
+		lc.addString("P", " AE ");
+		lc.addString("p", " AF ");
+		
+		lc.addString("Q", " B1 ");
+		lc.addString("q", " B2 ");
+		
+		lc.addString("R", " B3 ");
+		lc.addString("r", " B4 ");
+		
+		lc.addString("S", " B5 ");
+		lc.addString("s", " B6 ");
+		
+		lc.addString("T", " B7 ");
+		lc.addString("t", " B8 ");
+		
+		lc.addString("U", " B9 ");
+		lc.addString("u", " BA ");
+		
+		lc.addString("V", " BC ");
+		lc.addString("v", " BD ");
+		
+		lc.addString("W", " BE ");
+		lc.addString("w", " BF ");
+		
+		lc.addString("X", " C1 ");
+		lc.addString("x", " C2 ");
+		
+		lc.addString("Y", " C3 ");
+		lc.addString("y", " C4 ");
+		
+		lc.addString("Z", " C5 ");
+		lc.addString("z", " C6 ");
+		
+		lc.addString("H", " C7 ");
+		lc.addString("h", " C8 ");
+		
+		lc.addString(";", " C9 ");
+		lc.addString(":", " CA ");
+		
+		lc.addString("\"", " CB ");
+		lc.addString("\'", " CD ");
+		
+		lc.addString(".", " CE ");
+		lc.addString(",", " CF ");
+		
+		lc.addString(" ", " D1 ");
+		lc.addString("_", " D2 ");
+		
+		lc.addString("-", " D3 ");
+		lc.addString(" ", " D4 ");
+		lc.addString("\t", " D5 ");
+		
+		lc.addString("#", " D6 ");
+		lc.addString("+", " D7 ");
+		
+		lc.addString("*", " D8 ");
+		lc.addString("/", " D9 ");
+		lc.addString("\\", " DA ");
+		
+		lc.addString("?", " DB ");
+		lc.addString("!", " DC ");
+		
+		lc.addString("§", " DE ");
+		lc.addString("$", " DD ");
+		lc.addString("%&", " DF ");
+		
+		lc.addString(")", " E1 ");
+		lc.addString("(", " E2 ");
+		
+		lc.addString("=", " E3 ");
+		
+		lc.addString("}", " E4 ");
+		lc.addString("[", " E5 ");
+		
+		lc.addString("{", " E6 ");
+		lc.addString("]", " E7 ");
+		
+		lc.addString("~", " E8 ");
+		lc.addString("<", " E9 ");
+		lc.addString(">", " EA ");
+		lc.addString("|", " EB ");
+		
+		lc.addString("0", " EC ");
+		lc.addString("@", " ED ");
+		lc.addString("E", " EF ");*/
+		
+		/*lc.addString("", "EG ");
+		lc.addString("", "F1 ");
+		lc.addString("", "F2 ");
+		lc.addString("", "F3 ");*/
 	}
 	
 	public LCompiler getCompiler()
